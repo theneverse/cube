b0f9f9298745998cba4d715b5903faf3575ce104
Aiden X <caichao.xu@gmail.com>
2022-09-07 02:09:36 +0800

    Add media

---
+++ media/multipart.go
@@ -0,0 +1,26 @@
+package helper
+
+import (
+	"io"
+	"mime/multipart"
+	"strings"
+)
+
+func GetFileReaderAndType(resource *multipart.FileHeader) (io.Reader, string, error) {
+	reader, err := resource.Open()
+	if err != nil {
+		return nil, "", err
+	}
+
+	fileType := resource.Header.Get("Content-Type")
+	arr := strings.Split(fileType, "/")
+	if len(arr) < 2 {
+		return nil, "", err
+	}
+
+	if strings.Contains(arr[1], "svg") {
+		arr[1] = "svg"
+	}
+
+	return reader, arr[1], nil
+}
